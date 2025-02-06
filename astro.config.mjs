75ca57ac6cdbfc94316036929f0f4d330e4ca169
Dany <danfisch66@gmail.com>
2025-02-06 15:09:01 -0300

    Finished language selector in English

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,17 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
+// import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+
+  i18n: {
+    defaultLocale: "es",
+    locales: ["es", "en"],
+    routing: {
+      prefixDefaultLocale: false,
+    },
+  },
 });
